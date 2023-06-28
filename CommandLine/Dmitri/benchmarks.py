f83d6f52155d554da6556a157552e0bd56318d7c
Dmitri Svetlov <Dmitri.Svetlov@ColoState.edu>
2023-06-27 18:22:08 -0600

    Add draft code to compare ode23s to RK23

--- CommandLine/Dmitri/benchmarks.py
+++ CommandLine/Dmitri/benchmarks.py
@@ -1,11 +1,12 @@
 import numpy as np
 from matplotlib import pyplot as plt
-from scipy.sparse import csr_array, diags, sparray
+from scipy.integrate import solve_ivp
+from scipy.sparse import csr_array, diags#, sparray
 from scipy.sparse.linalg import expm_multiply
 import timeit
 
 from fspErrorCondition import FSPErrorCondition
-import mexpv_modified_2
+from mexpv_modified_2 import mexpv_modified_2
 
 def get_threshold(array1D: np.ndarray):
     sortedArray = np.sort(array1D)
@@ -14,7 +15,7 @@ def get_threshold(array1D: np.ndarray):
     else:
         return sortedArray[-5]
     
-def test_mexpv(t: int, A: sparray, v: np.ndarray):
+def test_mexpv(t: int, A: csr_array, v: np.ndarray) -> np.ndarray:
     m = 15
     tryAgain = True
     Time_array = np.array([0, t])
@@ -50,6 +51,9 @@ def test_mexpv(t: int, A: sparray, v: np.ndarray):
             
         m += 5
     # while tryAgain ...
+    
+    return ye
+# test_mexpv ...    
     
 numArrayDimensions = 6
 numSimulations = 5
@@ -102,33 +106,69 @@ for index in range(len(arrayDimensions) - 1, -1, -1):
         P0 = rng.random((N, 1))
         P0 = P0 / P0.sum()
         
-        # Solve the ODE via expm_multiply and record the time required in the 
-        # appropriate matrix. (We refer to the operation as "expm," but scipy
-        # provides an expm_multiply that includes the taking of the dot product
-        # of e^A and P0.)
+        # Solve the ODE via expm_multiply and record the solution and time
+        # required in the appropriate matrices. (We refer to the operation as
+        # "expm," but scipy provides an expm_multiply that includes the taking
+        # of the dot product of e^A and P0.) We will need to do this twice, 
+        # because generated variables do not persist after timeit is executed:
         
         timeExpm[simCntr][index] = timeit.timeit(
             stmt = """expm_multiply(A, P0, start = startPt,
             stop = startPt + numPt,
             num = numPt, endpoint = False)""", number = 1, globals = globals())
+        
+        expAt_P = expm_multiply(A, P0, start = startPt,
+                                stop = startPt + numPt,
+                                num = numPt, endpoint = False)
 
-        # Solve the ODE via a modified version of expokit and record the time 
-        # required in the appropriate matrix.
+        # # Solve the ODE via a modified version of expokit and record the 
+        # # solution and time required in the appropriate matrices. We will need
+        # # to do this twice, because generated variables do not persist after 
+        # # timeit is executed:
         
-        timeExpokit[simCntr][index] = timeit.timeit(
-            stmt = "test_mexpv(t = t, A = A, v = P0)", number = 1,
-            globals = globals())
+        # timeExpokit[simCntr][index] = timeit.timeit(
+        #     stmt = "test_mexpv(t = numPt, A = A, v = P0)", number = 1,
+        #     globals = globals())
         
-        # %% ODE Calculation
-        # ode_opts = odeset('Jacobian', A, 'Vectorized','on','JPattern',A~=0,'relTol',1e-8, 'absTol', 1e-10);
-        # rhs = @(t,x)A*x;
-        # tic
-        # [tExport, yout] =  ode15s(rhs, [0,t/2,t], P0);
-        # timeODE23s(j,index) = toc;
+        # PfExpokit = test_mexpv(t = numPt, A = A, v = P0)
+        
+        # Solve the ODE via the solve_ivp function provided by SciPy. Record
+        # the solution and time required in the appropriate matrices. We will 
+        # need to do this twice, because generated variables do not persist 
+        # after timeit is executed:
+            
+        def rhs(t, y): return A @ y
+        t_span = [0, numPt]
+        y0 = P0.flatten()
+        method = 'RK23'
+        t_eval = [0, numPt / 2, numPt]
+        rtol = 1e-8
+        atol = 1e-10
+        # The SciPy documentation states that vectorization will slow down
+        # finite-difference approximation for all methods other than Radau and
+        # BDF...
+        vectorized = False
+        jac = A
+        
+        stmt = """solve_ivp(fun = rhs, t_span = t_span, y0 = y0,
+        method = method, t_eval = t_eval, vectorized = vectorized,
+        rtol = rtol, atol = atol, jac = A)"""
+        
+        timeODE23s[simCntr][index] = timeit.timeit(
+            stmt = stmt, number = 1, globals = globals())
+        
+        sol = solve_ivp(fun = rhs, t_span = t_span, y0 = y0, method = method,
+                        t_eval = t_eval, vectorized = vectorized,
+                        rtol = rtol, atol = atol, jac = A)
+        
+        tExport = sol.t
+        yout = sol.y
 
         # diff_expm_expokit(j,index) = sum(abs(PfExpokit-expAt_P));
-        # diff_expm_ode23s(j,index) = sum(abs(expAt_P-yout(end,:)'));
+        diff_expm_ode23s[simCntr][index] = np.sum(
+            np.abs(expAt_P - yout[-1, :].T))
         # diff_expokit_ode23s(j,index) = sum(abs(PfExpokit-yout(end,:)'));
+    # for simCntr in range(numSimulations) ...
 
     # We will now generate figures comparing the running times and results of
     # the various ODE solvers tested.
@@ -136,13 +176,22 @@ for index in range(len(arrayDimensions) - 1, -1, -1):
     # Running times
     # figure(2)
     # subplot(2,1,1);
+    
     meanTimeExpm = np.mean(timeExpm, axis = COL_AXIS)
-    meanTimeExpokit = np.mean(timeExpokit, axis = COL_AXIS)
+    # meanTimeExpokit = np.mean(timeExpokit, axis = COL_AXIS)
+    meanTimeODE23s = np.mean(timeODE23s, axis = COL_AXIS)
+    
     np.savetxt('meanTimeExpm.txt', meanTimeExpm)
-    np.savetxt('meanTimeExpokit.txt', meanTimeExpokit)
+    # np.savetxt('meanTimeExpokit.txt', meanTimeExpokit)
+    np.savetxt('meanTimeODE23s.txt', meanTimeODE23s)
+    
     np.savetxt('arrayDimensions.txt', arrayDimensions)
-    plt.loglog(arrayDimensions, meanTimeExpm, '-s', label = 'expm')
-    plt.loglog(arrayDimensions, meanTimeExpm, '-o', label = 'expokit')
+    
+    plt.loglog(arrayDimensions, meanTimeExpm, 'b-o', label = 'expm')
+    # plt.loglog(arrayDimensions, meanTimeExpokit, '-o', label = 'expokit')
+    plt.loglog(arrayDimensions, meanTimeODE23s, 'r--', label = 'ode23s')
+    plt.loglog(arrayDimensions, meanTimeODE23s, 'r*')
+    
     # loglog(arrayDimensions,mean(timeExpm),'-s',arrayDimensions,mean(timeExpokit),'-o',arrayDimensions,mean(timeODE23s),'-^');
     # legend('expm','expokit','ode23s')
     plt.xlabel('Number of states')
